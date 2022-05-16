15466beae04abdb0ba9d5a644a1ef09a3b6a0edb
Vuskk5 <44120337+Vuskk5@users.noreply.github.com>
2022-05-16 14:25:10 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -9,4 +9,4 @@ def pytest_sessionstart(session: pytest.Session):
 
     # Initialize 'Step' plugin
     if hasattr(pytestconfig, 'py_test_service'):
-        Step.pytest_service = pytestconfig.py_test_service
+        Step.pytest_service = getattr(pytestconfig, 'py_test_service')
